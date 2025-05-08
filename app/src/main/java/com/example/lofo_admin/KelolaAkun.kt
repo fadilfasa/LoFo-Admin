d8d9eb1f05acb5845bc351e51b7d8b23a3cc793a
fadilfasa <fadilfasa03@gmail.com>
2025-05-08 17:13:18 +0800

    tampilan

---
+++ app/src/main/java/com/example/lofo_admin/KelolaAkun.kt
@@ -0,0 +1,15 @@
+package com.example.lofo_admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class KelolaAkun : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_kelola_akun)
+    }
+}
